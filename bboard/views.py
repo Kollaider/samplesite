1cb23d872b7853ad73146ace913d10511b978a54
Nikolay Strom <kolja87@tut.by>
2021-06-17 11:09:26 +0300

    First template index.html

--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from  django.http import HttpResponse
+from django.template import loader
+
+from .models import Bb
 
 def index(request):
-    return HttpResponse('Здесь будет выведен список объявлений')
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))
