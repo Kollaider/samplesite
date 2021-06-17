f68950a775b5368b48e6ff14777fdbbb2a2fd02c
Nikolay Strom <kolja87@tut.by>
2021-06-17 14:02:42 +0300

    templates refactoring, added base.html

--- bboard/views.py
+++ bboard/views.py
@@ -20,3 +20,18 @@ def by_rubric(request, rubric_id):
                'current_rubric': current_rubric
                }
     return render(request, 'bboard/by_rubric.html', context)
+
+
+from django.views.generic.edit import CreateView
+
+from .forms import BbForm
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = '/bboard'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
